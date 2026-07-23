ebb3afffcf8331b9cdb306795c5d9ba7db6ab539
Pavamana15 <124121665+Pavamana15@users.noreply.github.com>
2026-07-23 23:10:32 +0530

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-3.py
@@ -0,0 +1,69 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        
+        # def LCA(child):
+        #     if child is None:
+        #         return None
+
+        #     if child == p or child == q:
+        #         return child
+
+        #     left = LCA(child.left)
+        #     right = LCA(child.right)
+
+        #     if left and right:
+        #         return child
+
+        #     if left:
+        #         return left
+
+        #     if right:
+        #         return right
+
+        #     return None
+
+        # curr = p
+
+        # while curr.parent:
+        #     curr = curr.parent
+
+        # root = curr
+
+        # return LCA(root)
+
+
+        parents = set()
+
+        curr = p
+
+        while curr.parent:
+            parents.add(curr)
+            curr = curr.parent
+            
+        curr = q 
+        while curr.parent:
+            if curr in parents:
+                return curr
+            parents.add(curr)
+            curr = curr.parent
+
+        return curr
+
+
+        
+
+
+
+
+        
+        
